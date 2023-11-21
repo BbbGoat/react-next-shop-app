fc8194a32e7d6136d6bab96e2e557ae845c090c5
BbbGoat <oomi9421@gmail.com>
2023-11-21 19:05:17 +0900

    ss

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -29,7 +29,8 @@ const Chart = () => {
     datasets: [
       {
         label: '주문건수',
-        data: [1, 2, 3, 7],
+        // data: [placed, processing, shipped, deliverd],
+        data: [1,2,3,4],
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       }
     ]
@@ -43,16 +44,31 @@ const Chart = () => {
       },
       title: {
         display: true,
-        text: 'Delivery Service Bar Chart'
+        text: 'Delivery Status'
       }
     }
   }
 
+  // 주문내역 가져오기
+  // const orders = 
+  
+  // => 배송상태만 새 배열에 담기
+  const array: string[] = [];
+  // orders.map((order)=>{
+  //   const {orderStatus} = order;
+  //   array.push(orderStatus);
+  // })
+
   const getOrderStatusCount = (arr: string[], value: string) => {
     return arr.filter((n) => n === value).length;
   }
+
   const [x1, x2, x3, x4] = ["주문수락", "주문처리중", "배송중", "배송완료"]
 
+  const placed = getOrderStatusCount(array, x1);
+  const processing = getOrderStatusCount(array, x2);
+  const shipped = getOrderStatusCount(array, x3);
+  const deliverd = getOrderStatusCount(array, x4);
   
   return (
     <div className={styles.charts}>
