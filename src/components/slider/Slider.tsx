80b777c06ebd376835989aaf129f94eeb7930845
BbbGoat <oomi9421@gmail.com>
2023-11-28 21:40:06 +0900

    ss

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -5,8 +5,7 @@ import styles from './Slider.module.scss'
 import sliderData from './SliderData'
 import Image from 'next/image'
 import img from '@/assets/images/slider-img1.jpg'
-import Link from 'next/link';
-
+import Link from 'next/link'
 
 // 스와이퍼 라이브러리
 import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
@@ -36,18 +35,30 @@ const Slider = () => {
     <div className={styles.slider}>
 
       <Swiper
-        className={styles.mySwiper}
+        className={styles.mainSwiper}
         modules={[Navigation, Pagination, A11y, Autoplay]}
         slidesPerView={1}
         loop={true}
         speed={400}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        // autoplay={{ delay: 3000, disableOnInteraction: false }}
         onActiveIndexChange={(e)=>setSwiperIndex(e.realIndex)}
         onSwiper={(e) => {setSwiper(e)}}
       >
         <SwiperSlide>
           <Link href={'/'}>
             <Image src={img} alt='이미지' />
+
+            <div className={styles.titWrap}>
+              <span>PRE-ORDER</span>
+              <div className={styles.titInner}>
+                <div className={styles.title}>
+                  <strong>타이틀 이름 이름</strong>
+                </div>
+                <div className={styles.description}>
+                  <p>설명하는 내용 설명하는 내용 설명하는 내용asdfsadf</p>
+                </div>
+              </div>
+            </div>
           </Link>
         </SwiperSlide>
         <SwiperSlide>
@@ -67,13 +78,15 @@ const Slider = () => {
         </SwiperSlide>
       </Swiper>
       
-      <button onClick={handlePrev}>이전버튼</button>
-      <div>
-        <strong>{swiperIndex + 1}</strong>
-        {' / '}
-        <strong>드디어!마참내</strong>     
+      <div className={styles.navWrap}>
+        <button onClick={handlePrev} className={`${styles.swiperBtn} ${styles.prevBtn}`}></button>
+        <div className={styles.pagination}>
+          <span>{swiperIndex + 1}</span>
+          <span className={styles.divider}></span>
+          <span>6</span>     
+        </div>
+        <button onClick={handleNext} className={`${styles.swiperBtn} ${styles.nextBtn}`}></button>
       </div>
-      <button onClick={handleNext}>다음버튼</button>
 
     </div>
   )
