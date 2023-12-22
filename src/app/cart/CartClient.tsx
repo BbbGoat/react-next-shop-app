cc96519b9a00558e62adb94ea269a244ca787e5c
BbbGoat <oomi9421@gmail.com>
2023-12-22 12:25:39 +0900

    ss

---
+++ src/app/cart/CartClient.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import styles from './CartClient.module.scss'
+
+const CartClient = () => {
+
+    
+    
+  return (
+    <div>CartClient</div>
+  )
+}
+
+export default CartClient
