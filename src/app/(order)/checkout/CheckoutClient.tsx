5140252c4d8dab71d0a4f1f00a65bbc49d8f7e61
BbbGoat <oomi9421@gmail.com>
2023-12-25 18:17:13 +0900

    ss

--- src/app/(order)/checkout/CheckoutClient.tsx
+++ src/app/(order)/checkout/CheckoutClient.tsx
@@ -1,17 +1,33 @@
-'use client'
-import React from 'react'
-import styles from './CheckoutClient.module.scss'
+"use client";
+import React, { FormEvent } from "react";
+import styles from "./CheckoutClient.module.scss";
+import Heading from "@/components/heading/Heading";
+import Button from "@/components/button/Button";
+import CheckoutForm from "@/components/checkoutForm/CheckoutForm";
 
 const CheckoutClient = () => {
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  }
+  
   return (
     <section>
-        <div>
-            <form action="">
-                
-            </form>
-        </div>
+      <div className={styles.checkout}>
+        <Heading title="주문하기" />
+        <form onSubmit={handleSubmit}>
+          <div className={styles.card}>
+            <CheckoutForm />
+          </div>
+          <div>
+            <Button type="submit">
+              토스를 이용해서 결제하기
+            </Button>
+          </div>
+        </form>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default CheckoutClient
+export default CheckoutClient;
---
+++ src/components/checkoutForm/CheckoutForm.tsx
@@ -0,0 +1,65 @@
+'use client'
+import React, { useEffect } from 'react'
+import styles from './CheckoutForm.module.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { CALCULATE_TOTAL_AMOUNT, CALCULATE_TOTAL_QUANTITY, selectCartItems, selectCartTotalAmount, selectCartTotalQuantity } from '@/redux/slice/cartSlice'
+import Link from 'next/link'
+import priceFormat from '@/utils/priceFormat'
+
+// 카트 정보 가져와서 세팅하는 컴포넌트
+const CheckoutForm = () => {
+
+    // 카트 정보 가져오기
+    const cartItems = useSelector(selectCartItems);
+    const cartTotalQuantity = useSelector(selectCartTotalQuantity);
+    const cartTotalAmount = useSelector(selectCartTotalAmount);
+
+    const dispatch = useDispatch();
+
+    // 안전하게 스토어 데이터 한번 더 계산시키기
+    useEffect(()=>{
+        dispatch(CALCULATE_TOTAL_AMOUNT());
+        dispatch(CALCULATE_TOTAL_QUANTITY());
+    }, [dispatch, cartItems])
+    
+  return (
+    <div className={styles.summary}>
+        <h3>주문 요약</h3>
+        <div>
+            {
+                cartItems.length === 0 ? (
+                    <>
+                        <p>장바구니에 상품이 없습니다.</p>
+                        <Link href={'/'}>홈 페이지로</Link>
+                    </>
+                ) : (
+                    <>
+                        <div>
+                            {cartItems.map((item)=>{
+                                const { id, name, salePrice, cartQuantity } = item;
+                                return(
+                                    <div key={id} className={styles.card}>
+                                        <p><b>상품:</b>{name}</p>
+                                        <p><b>개수:</b>{cartQuantity}</p>
+                                        <p><b>판매가:</b>{priceFormat(salePrice)}원</p>
+                                        <p><b>합계:</b>{priceFormat(salePrice * cartQuantity)}원</p>
+                                    </div>
+                                )
+                            })}
+
+                            <div className={styles.text}>
+                                <p><b>총 수량:</b>{cartTotalQuantity}개</p>
+                            </div>
+                            <div className={styles.text}>
+                                <p><b>총 결제금액:</b>{priceFormat(cartTotalAmount)}원</p>
+                            </div>
+                        </div>
+                    </>
+                )
+            }
+        </div>
+    </div>
+  )
+}
+
+export default CheckoutForm
