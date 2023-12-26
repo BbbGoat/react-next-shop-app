a052c0c1d25136a629c042d5ad167a15420fce2d
BbbGoat <oomi9421@gmail.com>
2023-12-26 19:27:25 +0900

    ss

---
+++ src/app/(order)/order-details/[id]/OrderDetailsClient.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+import styles from './OrderDetails.module.scss'
+
+const OrderDetailsClient = () => {
+  return (
+    <div>OrderDetailsClient</div>
+  )
+}
+
+export default OrderDetailsClient
---
+++ src/app/(order)/order-details/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import OrderDetailsClient from './OrderDetailsClient'
+
+const OrderDetails = () => {
+  return (
+    <OrderDetailsClient />
+  )
+}
+
+export default OrderDetails
