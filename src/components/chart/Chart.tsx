f7cac8aff0f37c42be76028e3d6f3831f3fdfa2d
BbbGoat <oomi9421@gmail.com>
2023-12-28 15:49:01 +0900

    Update Chart.tsx

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -11,6 +11,8 @@ import {
   Legend,
 } from 'chart.js'
 import { Bar } from 'react-chartjs-2'
+import { useSelector } from 'react-redux'
+import { selectOrderHistory } from '@/redux/slice/orderSlice'
 
 ChartJS.register(
   CategoryScale,
@@ -21,16 +23,35 @@ ChartJS.register(
   Legend
 );
 
-
 const Chart = () => {
+
+  // 주문내역 가져오기
+  const orders = useSelector(selectOrderHistory);
+
+  // => 배송상태만 새 배열에 담기
+  const array: string[] = [];
+  orders.map((order)=>{
+    const {orderStatus} = order;
+    array.push(orderStatus);
+  })
+
+  const getOrderStatusCount = (arr: string[], value: string) => {
+    return arr.filter((n) => n === value).length;
+  }
+
+  const [x1, x2, x3, x4] = ["주문수락", "주문처리중", "배송중", "배송완료"]
+
+  const placed = getOrderStatusCount(array, x1);
+  const processing = getOrderStatusCount(array, x2);
+  const shipped = getOrderStatusCount(array, x3);
+  const deliverd = getOrderStatusCount(array, x4);
 
   const data = {
     labels: ["주문수락", "주문처리중", "배송중", "배송완료"],
     datasets: [
       {
         label: '주문건수',
-        // data: [placed, processing, shipped, deliverd],
-        data: [1,7,3,5],
+        data: [placed, processing, shipped, deliverd],
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       }
     ]
@@ -49,26 +70,9 @@ const Chart = () => {
     }
   }
 
-  // 주문내역 가져오기
-  // const orders = 
   
-  // => 배송상태만 새 배열에 담기
-  const array: string[] = [];
-  // orders.map((order)=>{
-  //   const {orderStatus} = order;
-  //   array.push(orderStatus);
-  // })
-
-  const getOrderStatusCount = (arr: string[], value: string) => {
-    return arr.filter((n) => n === value).length;
-  }
-
-  const [x1, x2, x3, x4] = ["주문수락", "주문처리중", "배송중", "배송완료"]
-
-  const placed = getOrderStatusCount(array, x1);
-  const processing = getOrderStatusCount(array, x2);
-  const shipped = getOrderStatusCount(array, x3);
-  const deliverd = getOrderStatusCount(array, x4);
+  
+  
   
   return (
     <div className={styles.charts}>
