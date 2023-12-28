ea13bcf4b28c2521d9f7d2add2b45eb05afe03d0
BbbGoat <oomi9421@gmail.com>
2023-12-28 21:18:33 +0900

    ss

--- src/app/(order)/order-details/[id]/OrderDetailsClient.tsx
+++ src/app/(order)/order-details/[id]/OrderDetailsClient.tsx
@@ -34,49 +34,58 @@ const OrderDetailsClient = () => {
             </p>
             <p>
               <b>주문 가격</b>
-              {order.orderAmount}
+              {order.orderAmount}원
             </p>
             <p>
               <b>주문 상태</b>
               {order.orderStatus}
             </p>
-            <table>
-              <thead>
-                <tr>
-                  <th>순서</th>
-                  <th>상품</th>
-                  <th>가격</th>
-                  <th>개수</th>
-                  <th>합계</th>
-                  <th>실행</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  order.cartItems.map((item: TCartItem, index: number)=>{
-                    const { id, name, salePrice, thumbnailURL, cartQuantity } = item;
-                    return (
-                      <tr key={id}>
-                        <td>{index + 1}</td>
-                        <td>
-                          <p><b>{name}</b></p>
-                          <Image src={thumbnailURL} alt={name} width={100} height={100} />
-                        </td>
-                        <td>{priceFormat(salePrice)}원</td>
-                        <td>{cartQuantity}</td>
-                        <td>{priceFormat(salePrice * cartQuantity)}원</td>
-                        <td className={styles.icons}>
-                          <Button onClick={()=>handleClick(id)}>
-                            상품 리뷰하기
-                          </Button>
-                        </td>
-                      </tr>
-                    )
-                  })
-                }
-              </tbody>
-            </table>
+            <p>
+              <b>배송 주소</b>
+              {order.shippingAddress.line}{" "}{order.shippingAddress.city}
+            </p>
           </div>
+          <table>
+            <thead>
+              <tr>
+                <th>순서</th>
+                <th>상품</th>
+                <th>가격</th>
+                <th>개수</th>
+                <th>합계</th>
+                <th>실행</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                order.cartItems.map((item: TCartItem, index: number)=>{
+                  const { id, name, brand, salePrice, thumbnailURL, cartQuantity } = item;
+                  return (
+                    <tr key={id}>
+                      <td>{index + 1}</td>
+                      <td className={styles.cell_pd}>
+                      <div className={styles.name}>
+                        <p>{brand}</p>
+                        <p>{name}</p>
+                      </div>
+                      <div className={styles.img}>
+                        <Image src={thumbnailURL} alt={name} width={100} height={100} />
+                      </div>
+                    </td>
+                      <td>{priceFormat(salePrice)}원</td>
+                      <td>{cartQuantity}</td>
+                      <td>{priceFormat(salePrice * cartQuantity)}원</td>
+                      <td className={styles.icons}>
+                        <Button onClick={()=>handleClick(id)}>
+                          상품 리뷰하기
+                        </Button>
+                      </td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
         </>
       )}
     </section>
