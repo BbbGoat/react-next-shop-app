c78c1d58bff68bf69ead490878df831d4bcae439
BbbGoat <oomi9421@gmail.com>
2023-12-28 15:45:10 +0900

    ss

--- src/app/admin/dashboard/DashboardClient.tsx
+++ src/app/admin/dashboard/DashboardClient.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './Dashboard.module.scss'
 import Heading from '@/components/heading/Heading'
 import InfoBox from '@/components/infoBox/InfoBox'
@@ -7,6 +7,11 @@ import { AiFillDollarCircle } from 'react-icons/ai'
 import { BsCart4 } from 'react-icons/bs'
 import { FaCartArrowDown } from 'react-icons/fa'
 import Chart from '@/components/chart/Chart'
+import { useDispatch, useSelector } from 'react-redux'
+import useFetchCollection from '@/hooks/useFetchCollection'
+import { CALCULATE_TOTAL_ORDER_AMOUNT, STORE_ORDERS, selectOrderHistory, selectTotalOrderAmount } from '@/redux/slice/orderSlice'
+import { STORE_PRODUCTS } from '@/redux/slice/productSlice'
+import priceFormat from '@/utils/priceFormat'
 
 const DashboardClient = () => {
 
@@ -14,6 +19,19 @@ const DashboardClient = () => {
     const earningIcon = <AiFillDollarCircle size={30} color='#b624ff' />;
     const productIcon = <BsCart4 size={30} color='#1f93ff' />;
     const ordersIcon = <FaCartArrowDown size={30} color='#4385F4' />;
+
+    const dispatch = useDispatch();
+    const products = useFetchCollection('products');
+    const { data } = useFetchCollection('orders');
+
+    const totalOrderAmount = useSelector(selectTotalOrderAmount);
+    const orders = useSelector(selectOrderHistory);
+    
+    useEffect(()=>{
+        dispatch(STORE_PRODUCTS({ products: products.data }))
+        dispatch(STORE_ORDERS(data));
+        dispatch(CALCULATE_TOTAL_ORDER_AMOUNT());
+    }, [dispatch, data, products])
     
   return (
     <div className={styles.home}>
@@ -22,19 +40,19 @@ const DashboardClient = () => {
             <InfoBox
                 cardClass={`${styles.card} ${styles.card1}`}
                 title={'수익'}
-                count={`원`}
+                count={`${priceFormat(Number(totalOrderAmount))}원`}
                 icon={earningIcon}
             />
             <InfoBox 
                 cardClass={`${styles.card} ${styles.card2}`}
                 title={'총 상품'}
-                count={`개`}
+                count={`${products.data.length}개`}
                 icon={productIcon}
             />
             <InfoBox 
                 cardClass={`${styles.card} ${styles.card3}`}
                 title={'총 주문건수'}
-                count={`건`}
+                count={`${orders.length}건`}
                 icon={ordersIcon}
             />
         </div>
