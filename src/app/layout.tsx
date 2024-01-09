17851a5d780491d0845737b2c4edbc30165df81a
BbbGoat <oomi9421@gmail.com>
2024-01-09 15:06:00 +0900

    SS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Providers from '@/redux/provider'
+import '@/fonts/fonts.css'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -21,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Providers>
           <Header />
           <ToastProvider />
